1e8298fab9a9e59d0bb5bc1abbc9b5dd2b8f158c
Movin Vinusandha <movinvinusandha@gmail.com>
2026-05-15 20:47:04 +0530

    Add Todo screen

---
+++ app/src/main/java/com/example/checkit/UserInfoActivity.java
@@ -0,0 +1,12 @@
+package com.example.checkit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("User Info");
+    }
+}
