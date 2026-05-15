fefc52d10c123077139971d46eac90b417369537
Movin Vinusandha <movinvinusandha@gmail.com>
2026-05-16 01:53:49 +0530

    Add Edit Profile screen

--- app/src/main/java/com/example/checkit/EditProfileActivity.java
+++ app/src/main/java/com/example/checkit/EditProfileActivity.java
@@ -1,12 +1,46 @@
 package com.example.checkit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
 
 public class EditProfileActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Edit Profile");
+        setContentView(R.layout.activity_edit_profile);
+
+        // Hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        ImageView ivBack = findViewById(R.id.iv_back);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        MaterialButton btnSave = findViewById(R.id.btn_save);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logic to save changes would go here
+                finish();
+            }
+        });
+
+        MaterialButton btnDiscard = findViewById(R.id.btn_discard);
+        btnDiscard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
