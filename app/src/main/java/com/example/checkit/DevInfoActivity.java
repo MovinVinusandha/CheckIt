9b2977050b1df995c471375c241e306aeba62800
Movin Vinusandha <movinvinusandha@gmail.com>
2026-05-16 00:27:44 +0530

    Add Developer Information screen

--- app/src/main/java/com/example/checkit/DevInfoActivity.java
+++ app/src/main/java/com/example/checkit/DevInfoActivity.java
@@ -1,12 +1,29 @@
 package com.example.checkit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DevInfoActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Developer Information");
+        setContentView(R.layout.activity_dev_info);
+
+        // Hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        ImageView ivBackProfile = findViewById(R.id.iv_back_profile);
+        ivBackProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Return to previous screen
+                finish();
+            }
+        });
     }
 }
